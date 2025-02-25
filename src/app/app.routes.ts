9edfb917a40c4339f6d8675538243410b3b7e973
Pedro Carné <68451062+pedroCConceicao@users.noreply.github.com>
2025-02-25 20:15:07 -0300

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { PedidoComponent } from './component/pedido/pedido.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: 'pedido', pathMatch: 'full' },
-    { path: 'pedido', component: PedidoComponent }
-];
--- src/app/model/Pedido.model.ts
+++
@@ -1,7 +0,0 @@
-export class Pedido {
-    codigoPedido: number | undefined;
-    dataPedido: Date | undefined;
-    codigoStatus: number | undefined;
-    codigoProduto: number | undefined;
-    codigoCliente: number | undefined;
-}
